3de50e7ca424fb9bc6d8fc553a206afe2c435b1d
SatishH64 <satishoro64@gmail.com>
2023-10-27 16:36:09 +0530

    uses unittest for functional testing

--- superlist/functional_tests.py
+++ superlist/functional_tests.py
@@ -1,7 +1,29 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert ("Congratulations!" in browser.title)
-print("OK")
+class NewVisitorTest(unittest.TestCase):
+    # Initialize webdriver
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    # Test Cases
+    def test_todo_list_start(self):
+        # Homepage of the website
+        self.browser.get("http://localhost:8000")
+
+        # Check title of Webpage
+        self.assertIn("To-Do", self.browser.title)
+
+        # This fails always
+        self.fail("Finish Test!")
+
+        # Quit Webdriver
+
+    def tearDown(self):
+        self.browser.quit()
+
+
+# Checks if imported or executed from command line
+if __name__ == "__main__":
+    unittest.main()
